bcb2b93128d595a4bf9e5e0a372f1c023a1acc24
mahedi066 <imam25349@gmail.com>
2024-04-17 19:12:14 +0600

    all is done

--- src/slider/Slider4.jsx
+++ src/slider/Slider4.jsx
@@ -24,7 +24,7 @@ const Slider4 = () => {
             <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
             <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
             <span className="relative transition-colors duration-200 delay-200 text-sm   group-hover:text-white ease">
-              Contact Now
+              register Now
             </span>
           </Link>
         </div>
